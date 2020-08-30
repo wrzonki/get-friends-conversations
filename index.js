f5b309261cb034d12023b462684af0db1802ce8b
wrzonki <wrzonki@gmail.com>
2020-08-30 03:16:57 +0200

    add model write

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const brain = require('brain.js')
 let rawdata = fs.readFileSync('training-data.json');
 let data = JSON.parse(rawdata);
 
-const net = new brain.NeuralNetwork({hiddenLayers :[3]});
+const net = new brain.recurrent.LSTM();
 
 
 const _Data = data;
+const config = {
+  iterations: 1,
+  log: true,
+  learningRate: 0.3,
+};
 net.train(_Data);
 console.log(net.run('Hello!'));
+
+fs.writeFileSync('trained-net.js', `export default ${ net.toFunction().toString() };`);
+
+// import trainedNet from './trained-net';
+// trainedNet(data);
